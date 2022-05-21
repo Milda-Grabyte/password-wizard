36aa20db5de1843a097bd45483d8a7733c28db5c
milda.grabyte_kairosds <milda.grabyte@kairosds.com>
2022-05-21 18:41:56 +0200

    feat: add footer buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 
-function Button() {
-  return <></>;
+function Button({ type }) {
+  const intl = useIntl();
+  return <input type='button' className={`button ${type}`} value=
+  {`${intl.formatMessage({ id: `footer.button.${type}` })}`} />
 }
 
 export default Button;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Button from './Button';
 import '../styles/footer.scss';
 
-function Footer({ step }) {
+function Footer({ step, success = true }) {
+  const buttons =
+    step !== 3 ? (
+      <>
+        <Button type='cancel' />
+        <Button type='next' />
+      </>
+    ) : (
+      <Button type={success === true ? 'ok' : 'ko'} />
+    );
   return (
     <footer>
-      <Button />
-      <Button />
-      FOOTER
+      {buttons}
     </footer>
   );
 }
